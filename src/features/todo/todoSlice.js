24ff9e3fbb18696eec93f7d443b0d461f8cd9990
chandima2000 <hychandima2000@gmail.com>
2024-08-13 22:47:23 +0530

    complete the "todoSlice" and export "todoReducer" functions

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,2 +1,35 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
 
+
+// Initial state
+const initialState = {
+    todos: [], //array
+}
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers:{
+
+        // Add Todo Reducer function
+        addTodo: (state, action) => {
+             const todo = {
+                id: nanoid(),
+                text: action.payload,
+             }
+             state.todos.push(todo) // push to the Store
+        },
+
+        // Delete Todo Reducer function
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload)  
+        },
+    }
+})
+
+// Export every reducer function one by one
+export const {addTodo, deleteTodo} = todoSlice.actions
+
+// Export entire reducer
+export default todoSlice.reducer
